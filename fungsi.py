afafc3a14b895d4ea02bc1b6a7a18d363ab675b3
betanarulita <91450916+betanarulita@users.noreply.github.com>
2023-06-01 15:59:14 +0700

    Update fungsi.py

--- fungsi.py
+++ fungsi.py
@@ -1,23 +1,29 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, MaxPool2D, Flatten, Dense, Activation, Dropout,LeakyReLU
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 
-def make_model():
+def make_model(train_data):
     model = Sequential()
     kernel = 3
     size_w = 64
-    size_h = size_w
+    size_h = 64
+    
     model.add(Conv2D(filters=64, kernel_size=kernel, input_shape=(size_w, size_h, 3), activation='relu'))
-    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
     model.add(Conv2D(filters=128, kernel_size=kernel, activation='relu'))
-    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
     model.add(Conv2D(filters=256, kernel_size=kernel, activation='relu'))
-    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
     model.add(Conv2D(filters=512, kernel_size=kernel, activation='relu'))
-    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
     model.add(Flatten())
     model.add(Dense(256, activation='relu'))
     model.add(Dropout(0.2))
-    model.add(Dense(20, activation='softmax'))
+
+    model.add(Dense(train_data.num_classes, activation='softmax'))
     
     return model
