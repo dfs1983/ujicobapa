6716fed126d2667ef2d58ad1ab4e2b810b7b9012
adindaauliaa <130948141+adindaauliaa@users.noreply.github.com>
2023-06-01 15:35:11 +0700

    Update fungsi.py

--- fungsi.py
+++ fungsi.py
@@ -1,88 +1,23 @@
-<<<<<<< HEAD
 import tensorflow as tf
-from tensorflow.keras.layers.experimental.preprocessing import Rescaling
-from tensorflow.keras.layers import RandomRotation, RandomZoom, Dense, GlobalAveragePooling2D
-from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.applications import EfficientNetB0
-from tensorflow.keras.losses import CategoricalCrossentropy
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, MaxPool2D, Flatten, Dense, Activation, Dropout,LeakyReLU
 
 def make_model():
-    BATCH_SIZE = 32
-    IMG_SIZE = (128, 128)
+    model = Sequential()
+    kernel = 3
+    size_w = 64
+    size_h = size_w
+    model.add(Conv2D(filters=64, kernel_size=kernel, input_shape=(size_w, size_h, 3), activation='relu'))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Conv2D(filters=128, kernel_size=kernel, activation='relu'))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Conv2D(filters=256, kernel_size=kernel, activation='relu'))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Conv2D(filters=512, kernel_size=kernel, activation='relu'))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(train_data.num_classes, activation='softmax'))
     
-    def data_augmentar():
-        data_augmentation = Sequential([
-            RandomRotation(factor=(-0.15, 0.15)),
-            RandomZoom(height_factor=(-0.3, -0.1))
-        ])
-
-        assert(data_augmentation.layers[0].name.startswith('random_rotation'))
-        assert(data_augmentation.layers[0].factor == (-0.15, 0.15))
-        assert(data_augmentation.layers[1].name.startswith('random_zoom'))
-        assert(data_augmentation.layers[1].height_factor == (-0.3, -0.1))
-
-        return data_augmentation
-
-    def alzheimer_classifier(image_shape=IMG_SIZE, data_augmentation=data_augmentar()):
-        image_shape = IMG_SIZE + (3,)
-        base_model = EfficientNetB0(input_shape=image_shape,
-                                    include_top=False, 
-                                    weights='imagenet')
-
-        base_model.trainable = True
-        for layer in base_model.layers[:218]:
-            layer.trainable = False
-
-        inputs = tf.keras.Input(shape=image_shape)
-        x = data_augmentation(inputs)
-        x = Rescaling(scale=1./255)(x)  # Rescaling input values
-        x = base_model(x, training=False)
-        x = GlobalAveragePooling2D()(x)
-        outputs = Dense(units=5, activation="softmax")(x)
-
-        model = Model(inputs, outputs)
-
-        return model
-    
-    alzheimer_model = alzheimer_classifier(IMG_SIZE, data_augmentar())
-
-    return alzheimer_model
-=======
-import pandas as pd
-import numpy as np
-
-import tensorflow
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.optimizers import Adam, SGD
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Input, Flatten, Dropout, UpSampling2D, GlobalAveragePooling2D
-from tensorflow.keras.models import Model
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
-
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import random, os
-
-def make_model_mobile():
-    img_size = 224
-    batch_size_train = 32
-    batch_size_test = 1
-    optimizer = Adam(learning_rate=0.00001)
-    
-    inp = Input(shape = (224,224,3))
-    model_mobile = MobileNetV2(input_shape=(224,224,3), include_top=False, weights='imagenet')
-    x1 = model_mobile(inp)
-    x2 = GlobalAveragePooling2D()(x1)
-    out = Dense(6, activation='softmax')(x2)
-    
-    model_mobile = Model(inputs = inp, outputs = out)
-    model_mobile.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
-    STEP_SIZE_TRAIN=train_generator.n//train_generator.batch_size
-    model_mobile.fit(train_generator,
-                     steps_per_epoch=STEP_SIZE_TRAIN,
-                     epochs=20,
-                     validation_data = test_generator
-    )
-    
-    return mobile_model
->>>>>>> 27fca911f37c2884c17a3ced190ec5f068f5268e
+    return model
