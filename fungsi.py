54dcd0298b6d22b051e250de550ae7a296cd4977
adindaauliaa <130948141+adindaauliaa@users.noreply.github.com>
2023-06-01 15:48:26 +0700

    Update fungsi.py

--- fungsi.py
+++ fungsi.py
@@ -7,11 +7,6 @@ def make_model():
     kernel = 3
     size_w = 64
     size_h = size_w
-    train_data = image_generator.flow_from_directory('/kaggle/input/punakawan/wayang/train',
-                                                 target_size=(size_w, size_h),
-                                                 batch_size=1,
-                                                 class_mode='categorical',
-                                                 color_mode='rgb')
     model.add(Conv2D(filters=64, kernel_size=kernel, input_shape=(size_w, size_h, 3), activation='relu'))
     model.add(MaxPool2D(pool_size=(2, 2)))
     model.add(Conv2D(filters=128, kernel_size=kernel, activation='relu'))
@@ -23,6 +18,6 @@ def make_model():
     model.add(Flatten())
     model.add(Dense(256, activation='relu'))
     model.add(Dropout(0.2))
-    model.add(Dense(train_data.num_classes, activation='softmax'))
+    model.add(Dense(20, activation='softmax'))
     
     return model
